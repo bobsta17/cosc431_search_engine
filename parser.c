31a222245f3219786c6f2344b24c94f996b76f53
Max Edwards <edwma571@student.otago.ac.nz>
2021-04-21 13:06:07 +1200

    Edited the code for finding tags to be very simple to account for all the unknown tags in the document

--- parser.c
+++ parser.c
@@ -2,19 +2,18 @@
     COSC431 Assignment 1: Parser
     Author: Max Edwards
     Date Created: 18/4/2020
-    Last Date Modified: 18/4/2020
+    Last Date Modified: 19/4/2020
 */
 #include <stdio.h>
-#include <stdbool.h>
 #include <ctype.h>
 #include <string.h>
 int main() {
     long docID = 0;
-    char currentWord[20];
+    char currentWord[50];
     int c, i, j;
     int letterIndex = 0;
     int isTag = 0;
-    char possibleTags[16][10] = {
+    char possibleTags[18][10] = {
         "doc",
         "docno",
         "hl",
@@ -30,12 +29,20 @@ int main() {
         "/so",
         "/in",
         "/dateline",
-        "/text"
+        "/text",
+        "docid",
+        "/docid"
     };
     for(;(c=getchar()) != EOF; letterIndex++) {
+        //if word is longer than currentWord limit, reset it.
+        if(letterIndex == (sizeof(currentWord)/sizeof(currentWord[0]))) {
+            letterIndex = -1;
+            continue;
+        }
         c = tolower(c);
         //Checks if we are in the middle of a tag
         if(isTag) {
+            /*
             //spaces will not occur in a tag in wsj.xml
             if(c == ' ') {
                 isTag = 0;
@@ -69,7 +76,17 @@ int main() {
                     }
                     continue;
                 }
+            }*/
+            currentWord[letterIndex] = (char) c;
+            if(c == '>') {
+                currentWord[letterIndex] = '\0';
+                if(strcmp("/doc", currentWord) == 0) {
+                    printf("\n");
+                }
+                isTag = 0;
+                letterIndex = -1;
             }
+            continue;
         }
         //Checks if we start looking at a tag
         if(c == '<') {
@@ -109,4 +126,5 @@ int main() {
         }
     }
     printf("\n");
+    return 0;
 }
