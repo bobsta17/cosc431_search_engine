756456b75852b56a18d923f871110c65b8fd81cb
Max Edwards <edwma571@student.otago.ac.nz>
2021-04-21 14:05:25 +1200

    Cleaned up the parser code and added some more comments

--- parser.c
+++ parser.c
@@ -8,31 +8,11 @@
 #include <ctype.h>
 #include <string.h>
 int main() {
-    long docID = 0;
     char currentWord[50];
-    int c, i, j;
+    int c;
     int letterIndex = 0;
     int isTag = 0;
-    char possibleTags[18][10] = {
-        "doc",
-        "docno",
-        "hl",
-        "dd",
-        "so",
-        "in",
-        "dateline",
-        "text",
-        "/doc",
-        "/docno",
-        "/hl",
-        "/dd",
-        "/so",
-        "/in",
-        "/dateline",
-        "/text",
-        "docid",
-        "/docid"
-    };
+    
     for(;(c=getchar()) != EOF; letterIndex++) {
         //if word is longer than currentWord limit, reset it.
         if(letterIndex == (sizeof(currentWord)/sizeof(currentWord[0]))) {
@@ -42,44 +22,10 @@ int main() {
         c = tolower(c);
         //Checks if we are in the middle of a tag
         if(isTag) {
-            /*
-            //spaces will not occur in a tag in wsj.xml
-            if(c == ' ') {
-                isTag = 0;
-            } else {
-                currentWord[letterIndex] = (char) c;
-                //Checking that it currently matches a tag
-                for(i=0;i<(sizeof(possibleTags)/sizeof(possibleTags[0]));i++) {
-                    for(j=0;j<letterIndex;j++) {
-                        if(possibleTags[i][j] != currentWord[j]) {
-                            break;
-                        }
-                    }
-                    if(j == letterIndex) {
-                        break;
-                    }
-                }
-                //This is only true if the current 'tag' doesnt match a known tag, hence is not a tag
-                //and will be treated as a normal word instead
-                if(j!= letterIndex) {
-                    isTag = 0;
-                }
-                else {
-                    //Dealing with the end of the tag
-                    if(c == '>') {
-                        currentWord[letterIndex] = '\0';
-                        if(strcmp("/doc", currentWord) == 0) {
-                            printf("\n");
-                        }
-                        isTag = 0;
-                        letterIndex = -1;
-                    }
-                    continue;
-                }
-            }*/
             currentWord[letterIndex] = (char) c;
             if(c == '>') {
                 currentWord[letterIndex] = '\0';
+                //checks we are at end of a document
                 if(strcmp("/doc", currentWord) == 0) {
                     printf("\n");
                 }
@@ -93,8 +39,10 @@ int main() {
             currentWord[letterIndex] = '\0';
             //checks current word isnt empty
             if(letterIndex != 0) {
+                //if it ends in a fullstop ignore the fullstop
                 if(currentWord[letterIndex-1] == '.') {
                     currentWord[letterIndex-1] = '\0';
+                    //if words was only a fullstop word is now empty so dont print it
                     if(letterIndex != 1) {
                         printf("%s ", currentWord);
                     }
@@ -104,15 +52,18 @@ int main() {
             }
             isTag=1;
             letterIndex=-1;
-        //Checks word has ended
-        } else if(c == ' ' || c== '\n' || c == '"' || c == ',' || c == '?' || c == '/' ||
+        //If any of these characters show up treat as end of word and reset word
+        } else if(c == ' ' || c== '\n' || c == '"' || c == ',' || c == '?' || c == '_' ||
                   c == '[' || c == ']' || c == '{' || c == '}' || c == ':' || c == ';' ||
-                  c == '?' || c == '>' || c == '(' || c == ')' || c == '&' || c == '!') {
+                  c == '?' || c == '>' || c == '(' || c == ')' || c == '&' || c == '!' ||
+                  c == '$') {
             currentWord[letterIndex] = '\0';
             //checks word isnt empty
             if(letterIndex != 0) {
+                //if word ends in fullstop ignore fullstop
                 if(currentWord[letterIndex-1] == '.') {
                     currentWord[letterIndex-1] = '\0';
+                    //checks if word is empty after removing fullstop
                     if(letterIndex != 1) {
                         printf("%s ", currentWord);
                     }
@@ -121,10 +72,10 @@ int main() {
                 }
             }
             letterIndex = -1;
+        //appends character to word
         } else {
             currentWord[letterIndex] = (char) c;
         }
     }
-    printf("\n");
     return 0;
 }
